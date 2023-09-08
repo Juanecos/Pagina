0fcde2a1929dcbb5af82dfe552fe4d603c7de6d1
juanecos <juanecos97@hotmail.com>
2023-09-08 12:06:55 -0500

    Agregados archivos no rastreados

---
+++ App1/static/miscript.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+      
+    const preferedColorScheme = window.matchMedia('prefers-color-scheme:dark').matches ? 'dark' : 'light';
+    const slider = document.getElementById('slider');
+
+    const setTheme = (theme) => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+
+
+    }
+    slider.addEventListener('change', ()=>{
+        let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+        setTheme(switchToTheme);
+
+    })
+
+    setTheme(localStorage.getItem('theme') || preferedColorScheme);
+
+});
---
+++ App1/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="inicio" ),
+    path('acerca_de/', views.about, name="acerca-de"),
+    path('observaciones/', views.reviews, name="observaciones"),
+    path('visualizacion/', views.view, name="visualizacion"),
+]
