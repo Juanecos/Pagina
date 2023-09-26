cb30d4083c52055b888a1db802e3504d46a8a668
juanecos <juanecos97@hotmail.com>
2023-09-26 12:33:37 -0500

    Inplementado temas a la nueva pagina

--- static/miscript.js
+++ static/miscript.js
@@ -3,17 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const preferedColorScheme = window.matchMedia('prefers-color-scheme:dark').matches ? 'dark' : 'light';
     const slider = document.getElementById('slider');
 
+
     const setTheme = (theme) => {
         document.documentElement.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
+
+        if (theme === 'dark') {
+            slider.checked = true; // Marcar el input si el tema es "dark"
+          } else {
+            slider.checked = false; // Desmarcar el input si el tema no es "dark" o no se encuentra en localStorage
+          }
+
     }
 
-    slider.addEventListener('click', ()=>{
+
+    slider.addEventListener('change', ()=>{
+
+
         let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
         setTheme(switchToTheme);
+        
+    
+
+    
+
     })
 
     setTheme(localStorage.getItem('theme') || preferedColorScheme);
+    
 
 });
 
